9ab1f944f42e3a28a7df1ca4200f4ada180a7f2d
Royd54 <duponroy@gmail.com>
2020-09-16 14:06:40 +0200

    Added player behaviour
    Made the player able to move, and made them able to spawn and join/host a game. Also started making some seat functions, so that I could easily implement the ball later. Spawning object's gets handled with RPC and command's (To and from the server). I chose this, because with this method every player sees the same things.

---
+++ Assets/scripts/player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    private float speed = 15f;
+    [SerializeField] private GameObject ball;
+    [SerializeField] private GameObject spawnPointBall;
+
+    [SyncVar] private Vector2 pos;
+
+    private void Update()
+    {
+        if (isLocalPlayer)
+        {
+            pos = transform.position;
+            transform.position = pos;
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-Vector2.right * speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && !GameObject.Find("Ball"))
+            {
+                CmdBallSpawnerServer();
+            }
+        }
+    }
+
+    [Command]
+    void CmdBallSpawnerServer()
+    {
+        GameObject ballToSpawn = Instantiate(ball, GameObject.Find("GameManager").GetComponent<Transform>().transform.position, Quaternion.identity) as GameObject;
+        NetworkServer.Spawn(ballToSpawn);
+    }
+
+
+    [ClientRpc]
+    void RpcBallSpawnerClient()
+    {
+        GameObject ballToSpawn = Instantiate(ball, GameObject.Find("GameManager").GetComponent<Transform>().transform.position, Quaternion.identity) as GameObject;
+    }
+}
