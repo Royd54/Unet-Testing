5398a5ccb6fbb0eade20d4a0add3e36662fa7768
Royd54 <duponroy@gmail.com>
2020-09-16 17:35:04 +0200

    Ball destruction
    When de ball hits either of the walls behind the player, the ball gets destroyed. After the destruction, the server gets a command to update the scores of the clients. After recieving the scores, the server sends a RPC back to the clients. This way the clients both have the same score on their screen.

---
+++ Assets/scripts/ball/DestroyBall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DestroyBall : NetworkBehaviour
+{
+    [SerializeField] private GameObject UIpanel;
+    [SerializeField] private GameObject PlayerScores;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            Destroy(collision.gameObject);
+            //GameObject.FindGameObjectWithTag("Player").GetComponent<playerMovement>().CmdhutsSpawner();
+            if (gameObject.tag == "Goal")
+            {
+                CmdScoreUpdatePlayer2();
+            }
+            else { CmdScoreUpdatePlayer1(); }
+            //UIpanel.GetComponent<Text>().text = PlayerScores.GetComponent<PlayerScores>().scorePlayer1 + " - " + PlayerScores.GetComponent<PlayerScores>().scorePlayer2;
+        }
+    }
+
+    [Command]
+    void CmdScoreUpdatePlayer1()
+    {
+        PlayerScores.GetComponent<PlayerScores>().scorePlayer1++;
+        RpcSetScores(PlayerScores.GetComponent<PlayerScores>().scorePlayer1, PlayerScores.GetComponent<PlayerScores>().scorePlayer2);
+    }
+
+    [Command]
+    void CmdScoreUpdatePlayer2()
+    {
+        PlayerScores.GetComponent<PlayerScores>().scorePlayer2++;
+        RpcSetScores(PlayerScores.GetComponent<PlayerScores>().scorePlayer1, PlayerScores.GetComponent<PlayerScores>().scorePlayer2);
+    }
+
+    [ClientRpc]
+    void RpcSetScores(int score1, int score2)
+    {
+        UIpanel.GetComponent<Text>().text = score1 + " - " + score2;
+    }
+
+}
