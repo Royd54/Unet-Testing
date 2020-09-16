2b4c2bb238f226c8903a0762cc3565adc58a2ad2
Royd54 <duponroy@gmail.com>
2020-09-16 20:08:30 +0200

    Fixed sync problems
    Had some syncing problems with the ball. After brainstorming for a bit, I managed to fix it, by sending commands and RPC's to the server and clients. So that everything has the same values. Before I did this, the ball sometimes took other angles if players had high latency/ping. This was caused by the ball not syncing properly. So I stopped using the UNET component, and started syncing with commands.

---
+++ Assets/scripts/ball/BallMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class BallMover : NetworkBehaviour
+{
+    [SerializeField] private GameObject mainball;
+
+    [SyncVar] public Vector2 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainball = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = mainball.GetComponent<BallBehaviour>().GetPos();
+        transform.position = pos;
+    }
+
+    [Command]
+    public void CmdUpdatePosServer(Vector2 pos)
+    {
+        this.transform.position = pos;
+        RpcUpdatePosClient(this.transform.position);
+    }
+
+    [ClientRpc]
+    public void RpcUpdatePosClient(Vector2 pos)
+    {
+        this.transform.position = pos;
+    }
+}
